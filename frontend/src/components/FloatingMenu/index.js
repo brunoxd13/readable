274b530d06918b27e2c29ad5493b6823923e1dfe
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-27 21:04:47 -0200

    style: margin vertical menu

--- frontend/src/components/FloatingMenu/index.js
+++ frontend/src/components/FloatingMenu/index.js
@@ -12,39 +12,41 @@ export default class FloatingMenu extends Component {
     const { activeItem } = this.state;
 
     return (
-      <Menu vertical className="menu-vertical">
-        <Menu.Item>
-          <Input placeholder="Search..." />
-        </Menu.Item>
+      <div className="menu-vertical-container">
+        <Menu vertical>
+          <Menu.Item>
+            <Input placeholder="Search..." />
+          </Menu.Item>
 
-        <Menu.Item>
-          Categories
-          <Icon name="grid layout" />
-          <Menu.Menu>
-            <Menu.Item
-              name="search"
-              active={activeItem === "search"}
-              onClick={this.handleItemClick}
-            >
-              Search
-            </Menu.Item>
-            <Menu.Item
-              name="add"
-              active={activeItem === "add"}
-              onClick={this.handleItemClick}
-            >
-              Add
-            </Menu.Item>
-            <Menu.Item
-              name="about"
-              active={activeItem === "about"}
-              onClick={this.handleItemClick}
-            >
-              Remove
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu.Item>
-      </Menu>
+          <Menu.Item>
+            Categories
+            <Icon name="grid layout" />
+            <Menu.Menu>
+              <Menu.Item
+                name="search"
+                active={activeItem === "search"}
+                onClick={this.handleItemClick}
+              >
+                Search
+              </Menu.Item>
+              <Menu.Item
+                name="add"
+                active={activeItem === "add"}
+                onClick={this.handleItemClick}
+              >
+                Add
+              </Menu.Item>
+              <Menu.Item
+                name="about"
+                active={activeItem === "about"}
+                onClick={this.handleItemClick}
+              >
+                Remove
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+        </Menu>
+      </div>
     );
   }
 }
