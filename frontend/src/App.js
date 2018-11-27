9f95ed41dcfb8a66c5f69e0c97e7e0f00f895a02
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-27 19:15:34 -0200

    refactor: recreated frontend project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">Batata</header>
-      </div>
-    );
+    return <div className="App">Hello-Worold</div>;
   }
 }
 
