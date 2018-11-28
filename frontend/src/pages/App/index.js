d7c9ca443d9bcff5bea483f6e1a81d26b546f0de
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-28 00:12:46 -0200

    feat: use store

--- frontend/src/pages/App/index.js
+++ frontend/src/pages/App/index.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import { fetchCategories } from "../../store/actions/categories";
+
 import FloatingMenu from "../../components/FloatingMenu";
 import HeaderMenu from "../../components/HeaderMenu";
 
-export default class App extends Component {
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchCategories());
+  }
+
   render() {
     return (
       <div>
         <HeaderMenu />
-        <FloatingMenu />
+        <FloatingMenu categories={this.props.categories} />
       </div>
     );
   }
 }
+function mapStateToProps({ categories }) {
+  return {
+    categories: categories
+  };
+}
+
+export default connect(mapStateToProps)(App);
