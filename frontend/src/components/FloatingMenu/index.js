f4bd8653e1696c92607924ba48e77bb5eed2ba19
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-28 12:02:24 -0200

    chore: removed styles

--- frontend/src/components/FloatingMenu/index.js
+++ frontend/src/components/FloatingMenu/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Icon, Input, Menu } from "semantic-ui-react";
 
-import "./styles.css";
-
 export default class FloatingMenu extends Component {
   state = {};
 
@@ -13,30 +11,28 @@ export default class FloatingMenu extends Component {
     const { categories } = this.props;
 
     return (
-      <div className="menu-vertical-container">
-        <Menu vertical>
-          <Menu.Item>
-            <Input placeholder="Search..." />
-          </Menu.Item>
+      <Menu vertical>
+        <Menu.Item>
+          <Input placeholder="Search..." />
+        </Menu.Item>
 
-          <Menu.Item>
-            Categories
-            <Icon name="grid layout" />
-            <Menu.Menu>
-              {Object.values(categories).map(category => (
-                <Menu.Item
-                  key={category.path}
-                  name={category.path}
-                  active={activeItem === category.path}
-                  onClick={this.handleItemClick}
-                >
-                  {category.name}
-                </Menu.Item>
-              ))}
-            </Menu.Menu>
-          </Menu.Item>
-        </Menu>
-      </div>
+        <Menu.Item>
+          Categories
+          <Icon name="grid layout" />
+          <Menu.Menu>
+            {Object.values(categories).map(category => (
+              <Menu.Item
+                key={category.path}
+                name={category.path}
+                active={activeItem === category.path}
+                onClick={this.handleItemClick}
+              >
+                {category.name}
+              </Menu.Item>
+            ))}
+          </Menu.Menu>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
