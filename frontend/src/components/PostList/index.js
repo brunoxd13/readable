2c00cc4756508490a38d86ddef5574ea19831a72
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-28 15:24:12 -0200

    refactor: changer component post list to use post

--- frontend/src/components/PostList/index.js
+++ frontend/src/components/PostList/index.js
@@ -1,44 +1,12 @@
 import React, { Component } from "react";
-import { Button, Icon, Divider, Item, Label } from "semantic-ui-react";
-
+import Post from "../Post";
 import "./styles.css";
 
 class PostList extends Component {
-  posts = () =>
-    Object.values(this.props.posts).map(post => {
-      return (
-        <div className="group-container">
-          <Item.Group>
-            <Item key={post.id}>
-              <Item.Content>
-                <Item.Header as="a">{post.title}</Item.Header>
-                <Item.Meta>
-                  <span>{post.author}</span>
-                </Item.Meta>
-                <Item.Description>{post.body}</Item.Description>
-                <Label>{post.category}</Label>
-                <Divider />
-                <Item.Extra>
-                  <Button.Group>
-                    <Button color="green" icon="thumbs up outline" />
-                    <Button.Or text={post.voteScore} />
-                    <Button color="red" icon="thumbs down outline" />
-                  </Button.Group>
-
-                  <Button primary floated="right">
-                    Continue Reading
-                    <Icon name="right chevron" />
-                  </Button>
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </div>
-      );
-    });
-
   render() {
-    return this.posts();
+    return Object.values(this.props.posts).map(post => {
+      return <Post post={post} isOnly={false} />;
+    });
   }
 }
 
