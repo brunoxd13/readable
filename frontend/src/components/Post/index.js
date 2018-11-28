c08162632ac8a5f1fdc97a7b05af1affc30d1bd3
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-28 15:23:52 -0200

    feat: created post component

---
+++ frontend/src/components/Post/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Button, Icon, Divider, Item, Label } from "semantic-ui-react";
+import { truncateString } from "../../service/utils";
+
+import "./styles.css";
+
+class Post extends Component {
+  render() {
+    const { post } = this.props;
+
+    return (
+      <div className="group-container">
+        <Item.Group>
+          <Item key={post.id}>
+            <Item.Content>
+              <Item.Header as="a">{post.title}</Item.Header>
+
+              <Item.Meta>
+                <span>
+                  <Icon name="user circle" />
+                  {post.author}
+                </span>
+
+                <span>
+                  <Icon name="comment" />
+                  {post.commentCount}
+                </span>
+              </Item.Meta>
+
+              <Item.Description>
+                {!this.props.isOnly
+                  ? truncateString(post.body, 200)
+                  : post.body}
+              </Item.Description>
+
+              <Label>{post.category}</Label>
+
+              <Divider />
+
+              <Item.Extra>
+                <Button.Group>
+                  <Button icon="thumbs up outline" />
+                  <Button.Or text={post.voteScore} />
+                  <Button icon="thumbs down outline" />
+                </Button.Group>
+
+                {!this.props.isOnly && (
+                  <Button secondary floated="right">
+                    Continue Reading
+                    <Icon name="right chevron" />
+                  </Button>
+                )}
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </div>
+    );
+  }
+}
+
+export default Post;
