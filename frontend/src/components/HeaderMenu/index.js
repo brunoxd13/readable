842bcc4690b9ece701d57d730f37c2f725fd64a9
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-11-29 14:38:07 -0200

    refactor: from component to stateless

--- frontend/src/components/HeaderMenu/index.js
+++ frontend/src/components/HeaderMenu/index.js
@@ -1,40 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import { Menu, Icon, Header } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-class HeaderMenu extends Component {
-  state = {};
+const HeaderMenu = () => (
+  <Menu stackable>
+    <Menu.Item name="features">
+      <Link to="/">Home</Link>
+    </Menu.Item>
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+    <Menu.Menu position="right">
+      <Menu.Item name="testimonials">About</Menu.Item>
 
-  render() {
-    const { activeItem } = this.state;
+      <Menu.Item name="sign-in">Sign-out</Menu.Item>
 
-    return (
-      <Menu stackable>
-        <Menu.Item name="features" onClick={this.handleItemClick}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-
-        <Menu.Menu position="right">
-          <Menu.Item name="testimonials" onClick={this.handleItemClick}>
-            About
-          </Menu.Item>
-
-          <Menu.Item name="sign-in" onClick={this.handleItemClick}>
-            Sign-out
-          </Menu.Item>
-
-          <Menu.Item>
-            <Header as="h6">
-              <Icon name="user circle" circular />
-              Udacity Student
-            </Header>
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-    );
-  }
-}
+      <Menu.Item>
+        <Header as="h6">
+          <Icon name="user circle" circular />
+          Udacity Student
+        </Header>
+      </Menu.Item>
+    </Menu.Menu>
+  </Menu>
+);
 
 export default HeaderMenu;
