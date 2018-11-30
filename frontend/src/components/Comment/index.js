0b15a8ffb452f8619eff8428b175a68ebd9c74dd
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2018-11-29 22:22:10 -0200

    feat: vote comment ui

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleVoteComment } from "../../store/actions/comments";
 import { Feed, Button, Segment, Divider } from "semantic-ui-react";
 
-const Comment = ({ comment }) => (
-  <Segment>
-    <Feed.Event>
-      <Feed.Content>
-        <Feed.Summary>
-          <Feed.User>{comment.author}</Feed.User> Commented
-          {/* <Feed.Date>1 Hour Ago</Feed.Date> */}
-        </Feed.Summary>
+class Comment extends Component {
+  handleClickUpVote = () => {
+    this.props.dispatch(handleVoteComment(this.props.comment.id, "upVote"));
+  };
 
-        <Feed.Extra text>{comment.body}</Feed.Extra>
-        <Divider />
-        <Feed.Meta>
-          <Feed.Like>
-            <Button.Group size="mini">
-              <Button icon="thumbs up" color="teal" />
-              <Button.Or text={comment.voteScore} />
-              <Button icon="thumbs down" color="red" />
-            </Button.Group>
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
-  </Segment>
-);
+  handleClickDownVote = () => {
+    this.props.dispatch(handleVoteComment(this.props.comment.id, "downVote"));
+  };
 
-export default Comment;
+  render() {
+    const { comment } = this.props;
+
+    return (
+      <Segment>
+        <Feed.Event>
+          <Feed.Content>
+            <Feed.Summary>
+              <Feed.User>{comment.author}</Feed.User> Commented
+              {/* <Feed.Date>1 Hour Ago</Feed.Date> */}
+            </Feed.Summary>
+
+            <Feed.Extra text>{comment.body}</Feed.Extra>
+            <Divider />
+            <Feed.Meta>
+              <Feed.Like>
+                <Button.Group size="mini">
+                  <Button
+                    icon="thumbs up"
+                    color="teal"
+                    onClick={this.handleClickUpVote}
+                  />
+                  <Button.Or text={comment.voteScore} />
+                  <Button
+                    icon="thumbs down"
+                    color="red"
+                    onClick={this.handleClickDownVote}
+                  />
+                </Button.Group>
+              </Feed.Like>
+            </Feed.Meta>
+          </Feed.Content>
+        </Feed.Event>
+      </Segment>
+    );
+  }
+}
+
+export default connect()(Comment);
