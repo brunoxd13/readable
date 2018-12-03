af3a4629958d7a0bffa8191febe8a7d4f6f99e4d
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-12-03 13:39:01 -0200

    fix: bug at new post route

--- frontend/src/components/Router/index.js
+++ frontend/src/components/Router/index.js
@@ -7,9 +7,9 @@ import NewPost from "../NewPost";
 const Router = () => (
   <Switch>
     <Route exact path="/" component={PostListContainer} />
+    <Route path="/new-post" component={NewPost} />
     <Route exact path="/:category" component={PostListContainer} />
     <Route path="/:category/:id" component={PostView} />
-    <Route path="/new-post" component={NewPost} />
     <Route component={() => <Redirect to={{ pathname: "/" }} />} />
   </Switch>
 );
